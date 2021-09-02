6aed8073f3be7b53822c5b932717df334e36d753
rakibshakib <rakibshakib74@gmail.com>
2021-09-02 11:52:47 +0600

    update all api and modify in the design section and add error message

--- script/app.js
+++ script/app.js
@@ -1,42 +1,60 @@
 const inputBookField = document.getElementById('input-book');
-const searchFound = document.getElementById('total-item');
 const displayBook = document.getElementById('display-book');
 const loading = document.getElementById('loading');
-const numFound = document.getElementById('total-num');
+const resultFound = document.getElementById('total-num');
+const errorMsg = document.getElementById('errorMsg');
+const notFound = document.getElementById('notFound-Item');
+const query = document.getElementById("query");
 
 
 const getBooks = async () => {
     loading.innerText = "Data Loading.......";
+    displayBook.innerHTML = '';
     const bookName = inputBookField.value;
     const url = `http://openlibrary.org/search.json?q=${bookName}`
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-        updateBookData(data);
-    } catch (error) {
-        console.log(error);
+    if (inputBookField.value === '') {
+        errorMsg.style.display = 'block';
+        loading.innerText = '';
     }
+    else {
+        errorMsg.style.display = 'none';
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            updateBookData(data);
+        } catch (error) {
+            console.log(error);
+            loading.innerText = "503 Error!! Data Not Found.";
+        }
+    }
+    inputBookField.value = '';
 };
 
 const updateBookData = data => {
-    displayBook.innerHTML = '';
-    searchFound.innerText = data.docs.length;
-    numFound.innerText = data.numFound;
-    data.docs.slice(0, 10).forEach(book => {
-        showData(book);
-    })
+    resultFound.innerText = data.numFound;
+    query.innerText = data.q;
+    if (data.numFound === 0) {
+        loading.innerText = '';
+        notFound.style.display = 'block';
+    } else {
+        data.docs.slice(0, 10).forEach(book => {
+            showData(book);
+        })
+    }
+
 }
 const showData = book => {
+    loading.innerText = '';
     const li = document.createElement('li');
-    li.classList.add("flex", "justify-start", "items-center", "border-2", "py-2", "px-5", "my-2", "bg-blue-200");
+    li.classList.add("flex", "justify-start", "items-center", "border-2", "py-2", "px-5", "my-2", "bg-green-100");
     li.innerHTML = `
         <figure>
-            <img class="w-40" src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" alt="image not found">
+            <img class="w-40" src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" alt="">
         </figure>
         <div class="ml-8">
-            <h2 class="text-3xl">${book.title}</h2>
-            <p class="text-xl">Author Name: ${book.author_name}</p>
-            <p>Publish Date: ${book.first_publish_year}</p>
+            <h2 class="text-4xl my-3">${book.title}</h2>
+            <p class="text-2xl mb-3">Author Name: ${book.author_name}</p>
+            <p>Publish Date In: ${book.first_publish_year}</p>
         </div>
         `
     displayBook.appendChild(li);
